82e0a764100078b2dd0c9265c486669e06796132
Pieter Deane <deanepieter@gmail.com>
2024-02-11 16:25:12 +0200

    added improvements

--- app/components/snake/page.jsx
+++ app/components/snake/page.jsx
@@ -1,23 +1,20 @@
-"use client"
-import { useState } from 'react';
-import { BOARD_SIZE } from '../gameboard/page';
+'use client';
 
-const INITIAL_SNAKE_LENGTH = 3;
-
-const Snake = () => {
-  const [snakeSegments, setSnakeSegments] = useState(() =>
-    Array.from({ length: INITIAL_SNAKE_LENGTH }, (_, i) => ({
-      x: Math.floor(BOARD_SIZE / 2),
-      y: Math.floor(BOARD_SIZE / 2) + i,
-    }))
-  );
-
+const Snake = ({ snakeSegments }) => {
   return (
     <>
       {snakeSegments.map((segment, index) => (
-        <div key={index} className="snake-segment w-10 h-10 bg-blue-500"></div>
+        <div
+          key={index}
+          className="snake-segment w-10 h-10 bg-blue-500"
+          style={{
+            gridColumnStart: segment.x + 1,
+            gridRowStart: segment.y + 1,
+          }}
+        />
       ))}
     </>
   );
 };
+
 export default Snake;
